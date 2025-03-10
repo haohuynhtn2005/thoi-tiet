cbe413aaf9f815e0a73c2e38605261b01f37eac6
Hao <192075106+haohuynhtn2005@users.noreply.github.com>
2025-03-10 21:09:18 +0700

    Update dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <Wrapper
       style={{
-        backgroundImage: 'url(/assets/background/overlay.jpg)',
+        // backgroundImage: 'url(/assets/background/overlay.jpg)',
         backgroundAttachment: 'fixed',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,9 +5,15 @@ const initialDarkModeVal = localStorage.getItem('darkMode') == 'true';
 function useDarkMode() {
   const [darkMode, setDarkModeState] = useState(initialDarkModeVal);
 
+  if (darkMode) {
+    document.body.setAttribute('data-bs-theme', 'dark')
+  } else {
+    document.body.setAttribute('data-bs-theme', 'light')
+  }
+
   function setDarkMode(darkModeValue = true) {
     setDarkModeState(darkModeValue);
-    localStorage.setItem('darkMode', String(darkModeValue));
+    localStorage.setItem('darkMode', darkModeValue);
   }
 
   return [darkMode, setDarkMode];
